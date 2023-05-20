5fa888a78abe1f32242b2bdabe2070c5b48b9372
andraziel <auxence.deleuziere@isen.yncrea.fr>
2023-05-20 16:30:35 +0200

    Right send to domain and send to cidr struct

--- icecrawlerui/src/app/shared-interfaces/send-to-cidr.ts
+++ icecrawlerui/src/app/shared-interfaces/send-to-cidr.ts
@@ -1,8 +1,4 @@
 export interface SendToCIDR
 {
-  //  If true CIDR
-  /*  "CIDR":string */  // De la forme X.X.X.X/Y
-
-  "StartIP": string     //De la forme X.X.X.X/Y
-  "EndIP": string       //De la forme X.X.X.X/Y
+  "CIDR":string  // De la forme X.X.X.X/Y
 }
