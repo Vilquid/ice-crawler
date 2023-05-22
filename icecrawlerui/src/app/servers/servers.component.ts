17f0c57c2ebbc839090364929637a2245fcb769a
mehdi.dupont <mehdi.dupont@isen.yncre.fr>
2023-05-22 14:43:33 +0200

    added csv domain search

--- icecrawlerui/src/app/servers/servers.component.ts
+++ icecrawlerui/src/app/servers/servers.component.ts
@@ -1,12 +1,12 @@
-import { DataColService } from '../data-col.service';
-import { Component, OnInit } from '@angular/core';
-import { UntypedFormGroup, FormControl, Validators } from '@angular/forms';
-import { CacheService } from '../cache.service';
+import {DataColService} from '../data-col.service';
+import {Component, OnInit} from '@angular/core';
+import {UntypedFormGroup, FormControl, Validators} from '@angular/forms';
+import {CacheService} from '../cache.service';
 import * as AOS from 'aos';
-import { DATAResult } from '../shared-interfaces/data-result';
-import { SendToDomain } from '../shared-interfaces/send-to-domain';
-import { SendToCIDR } from '../shared-interfaces/send-to-cidr';
-
+import {DATAResult} from '../shared-interfaces/data-result';
+import {SendToDomain} from '../shared-interfaces/send-to-domain';
+import {SendToCIDR} from '../shared-interfaces/send-to-cidr';
+import { Papa } from 'ngx-papaparse';
 
 
 @Component({
@@ -16,14 +16,14 @@ import { SendToCIDR } from '../shared-interfaces/send-to-cidr';
 })
 export class ServersComponent implements OnInit {
   servers: Array<DATAResult> = new Array<DATAResult>()
-  texterecherche:any
+  texterecherche: any
 
   domainForm: UntypedFormGroup///
-  domainCtrl = new FormControl<string>('', { nonNullable: true })///
+  domainCtrl = new FormControl<string>('', {nonNullable: true})///
   cidrForm: UntypedFormGroup///
-  cidrCtrl = new FormControl<string>('', { nonNullable: true })///
+  cidrCtrl = new FormControl<string>('', {nonNullable: true})///
   radioForm: UntypedFormGroup///
-  radioCtrl = new FormControl<string>('', { nonNullable: true })///
+  radioCtrl = new FormControl<string>('', {nonNullable: true})///
 
   server_test: DATAResult = {
     dns: {
@@ -134,65 +134,107 @@ export class ServersComponent implements OnInit {
   };
 
 
-  constructor(private dataCol: DataColService, private cache: CacheService) {
+  constructor(private dataCol: DataColService, private cache: CacheService, private papa: Papa) {
 
     this.domainForm = new UntypedFormGroup///
-        ({
+      ({
         domain: this.domainCtrl,
-        })
+      })
     this.cidrForm = new UntypedFormGroup///
-        ({
+      ({
         CIDR: this.cidrCtrl,
-        })
+      })
     this.radioForm = new UntypedFormGroup///
-        ({
+      ({
         radio: this.radioCtrl,
-        })
+      })
   }
 
   ngOnInit(): void {
 
     AOS.init({
-      duration:1100
+      duration: 1100
     });
 
 
     this.servers.push(this.server_test)
 
     this.radioForm.setValue({radio: "cidr"})
+  }
+
+  submitDomain() {////
+
+    let data: SendToDomain = {
+      "domain": this.domainForm.value.domain
     }
 
-      submitDomain() {////
-
-      let data: SendToDomain = {
-      "domain": this.domainForm.value.domain
-      }
-
-      this.dataCol.launchScanByDomain(data).subscribe(
+    this.dataCol.launchScanByDomain(data).subscribe(
       p => {
         console.log(p)
         this.servers = this.servers.concat(p)
       })
 
-    }
-    submitCIDR() {////
-      
-      let regex = new RegExp("^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(/(3[0-2]|2[0-9]|1[0-9]|[0-9]))$")
-      if (regex.test(this.cidrForm.value.CIDR)) {
-        let data: SendToCIDR = {
-        "CIDR": this.cidrForm.value.CIDR
-        }
-        console.log(data)
+  }
 
-        this.dataCol.launchScanByCIDR(data).subscribe(
+  submitCIDR() {////
+
+    let regex = new RegExp("^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(/(3[0-2]|2[0-9]|1[0-9]|[0-9]))$")
+    if (regex.test(this.cidrForm.value.CIDR)) {
+      let data: SendToCIDR = {
+        "CIDR": this.cidrForm.value.CIDR
+      }
+      console.log(data)
+
+      this.dataCol.launchScanByCIDR(data).subscribe(
         p => {
           console.log(p)
           this.servers = this.servers.concat(p)
         })
-      }
-      
-
     }
+
+
+  }
+
+  onFileSelected(event: any): void {
+    const file: File = event.target.files[0];
+
+    this.papa.parse(file, {
+      header: true,
+      complete: (result: any) => {
+        const data = result.data;
+        this.processCSV(data);
+      },
+      error: (error: any) => {
+        console.error('CSV parsing error:', error);
+        // Handle the error scenario, such as showing an error message to the user.
+      }
+    });
+  }
+
+
+  processCSV(data: any[]): void {
+    data.forEach((row: any) => {
+      // Assuming your CSV file has a column named 'domain' for the domain value
+      const domain = row.domain;
+
+      if (domain) {
+        const data: SendToDomain = {
+          domain: domain
+        };
+
+        this.dataCol.launchScanByDomain(data).subscribe(
+          p => {
+            console.log(p);
+            this.servers = this.servers.concat(p);
+          },
+          error => {
+            console.error('Error submitting domain:', error);
+            // Handle the error scenario, such as showing an error message to the user.
+          }
+        );
+      }
+    });
+  }
 
 
 }
