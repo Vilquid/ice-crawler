92cd35b39392b9def24b039574e07493135f2a53
paulctl74 <104615011+paulctl74@users.noreply.github.com>
2023-05-23 12:34:38 +0200

    Update mta_sts.rs

--- data_ms/src/mta_sts.rs
+++ data_ms/src/mta_sts.rs
@@ -16,7 +16,16 @@ pub struct MTARecord
 	#[serde(with = "serde_json::json")]
 	pub version: String,
 	pub sn: String,
+	#[serde(serialize_with = "serialize_f32_without_quotes")]
 	pub note: f32,
+}
+
+
+fn serialize_f32_without_quotes<S>(value: &f32, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    serializer.serialize_str(&value.to_string())
 }
 
 
