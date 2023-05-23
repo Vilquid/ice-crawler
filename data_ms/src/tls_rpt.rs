417563a88a2043e7bf8d7dac9b68aea7c3fc210f
paulctl74 <104615011+paulctl74@users.noreply.github.com>
2023-05-23 12:37:10 +0200

    Update tls_rpt.rs

--- data_ms/src/tls_rpt.rs
+++ data_ms/src/tls_rpt.rs
@@ -11,7 +11,15 @@ pub struct TLSRecord
 	#[serde(with = "serde_json::json")]
 	pub v: String,
 	pub rua: String,
+	#[serde(serialize_with = "serialize_f32_without_quotes")]
 	pub note: f32,
+}
+
+fn serialize_f32_without_quotes<S>(value: &f32, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    serializer.serialize_str(&value.to_string())
 }
 
 
