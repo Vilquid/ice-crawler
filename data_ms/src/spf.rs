fe799b29e63d9d197f1c34729d3861974f6d630b
paulctl74 <104615011+paulctl74@users.noreply.github.com>
2023-05-24 15:07:14 +0200

    Update spf.rs

--- data_ms/src/spf.rs
+++ data_ms/src/spf.rs
@@ -22,16 +22,10 @@ pub struct SPFRecord
 	pub ip: Vec<String>,
 	pub include: Vec<String>,
 	pub all: String,
-	#[serde(serialize_with = "serialize_f32_without_quotes")]
 	pub note: f32,
 }
 	
-fn serialize_f32_without_quotes<S>(value: &f32, serializer: S) -> Result<S::Ok, S::Error>
-where
-    S: Serializer,
-{
-    serializer.serialize_str(&value.to_string())
-}
+
 
 
 pub(crate) fn spf(domain: String) -> SPFRecord
