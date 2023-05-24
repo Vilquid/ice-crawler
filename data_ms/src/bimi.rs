d6edd317551033d11f6f77aa48f642da3ce18476
paulctl74 <104615011+paulctl74@users.noreply.github.com>
2023-05-24 15:04:13 +0200

    Update bimi.rs

--- data_ms/src/bimi.rs
+++ data_ms/src/bimi.rs
@@ -22,16 +22,10 @@ pub struct BIMIRecord
 	pub url_reputation: String,
 	pub hash: String,
 	pub s: String,
-	#[serde(serialize_with = "serialize_f32_without_quotes")]
 	pub note: f32,
 }
 
-fn serialize_f32_without_quotes<S>(value: &f32, serializer: S) -> Result<S::Ok, S::Error>
-where
-    S: Serializer,
-{
-    serializer.serialize_str(&value.to_string())
-}
+
 
 
 pub(crate) fn bimi(domain: String) -> BIMIRecord
